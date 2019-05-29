e8cd177b00c8c704cce1c4167ff8120602dc703b
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-05-29 23:39:20 +0500

    3 готова

--- Задача 3/Задача 3.cs
+++ Задача 3/Задача 3.cs
@@ -8,8 +8,28 @@ namespace Задача_3
 {
     class Program
     {
+        public static double Function(double x)
+        {
+            if (x < 0) return Math.Abs(x + 1);
+            else return Math.Abs(x - 1);
+        }
         static void Main(string[] args)
         {
+            double a = 0;
+            bool ok = false;
+            while (!ok)
+            {
+                Console.WriteLine("Введите число, для которого необходимо вычислить функцию");
+                try
+                {
+                    a = Convert.ToDouble(Console.ReadLine());
+                    ok = true;
+                }
+                catch { Console.WriteLine("Ошибка ввода"); }
+            }
+
+            Console.WriteLine("\nПолученное значение функции: " + Function(a));
+            Console.ReadKey();
         }
     }
 }
