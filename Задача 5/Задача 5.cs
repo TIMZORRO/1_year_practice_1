9b3fe37772b726e6a2346e442bad07a44aef7fab
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-05-30 11:41:17 +0500

    5 задача добавлен ввод из файла

--- Задача 5/Задача 5.cs
+++ Задача 5/Задача 5.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,33 +12,76 @@ namespace Задача_5
     {
         static void Main(string[] args)
         {
+            string input_f = "input.txt";
             int[,] spiral = new int[,] {    { 1, 2, 3, 4, 5, 6, 7 },
-                                            { 24, 25, 26, 27, 28, 29, 8 }, 
-                                            { 23, 40, 41, 42, 43, 30, 9 }, 
-                                            { 22, 39, 48, 49, 44, 31, 10 }, 
-                                            { 21, 38, 47, 46, 45, 32, 11 }, 
-                                            { 20, 37, 36, 35, 34, 33, 12 }, 
+                                            { 24, 25, 26, 27, 28, 29, 8 },
+                                            { 23, 40, 41, 42, 43, 30, 9 },
+                                            { 22, 39, 48, 49, 44, 31, 10 },
+                                            { 21, 38, 47, 46, 45, 32, 11 },
+                                            { 20, 37, 36, 35, 34, 33, 12 },
                                             { 19, 18, 17, 16, 15, 14, 13 } };
 
-            double[,] arr = new double[7,7];
+            double[,] arr = new double[7, 7];
             double[] ans = new double[49];
 
-            for (int i=0; i < arr.GetLength(0); i++)
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Выберите способ ввода: ");
+                Console.WriteLine("1 - ввод матрицы из файла");
+                Console.WriteLine("2 - ручной ввод");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        using (FileStream sf = new FileStream(input_f, FileMode.OpenOrCreate)) { }
+                        using (StreamReader reader = new StreamReader(input_f))
+                        {
+                            try
+                            {
+                                for (int i = 0; i < spiral.GetLength(0); i++)
+                                {
+                                    string str = reader.ReadLine();
+                                    str = Regex.Replace(str.Trim(), @"\s+", " ");
+                                    string[] chisla = str.Split(new char[] { ' ' });
+                                    for (int j = 0; j < spiral.GetLength(1); j++) arr[i, j] = Convert.ToDouble(chisla[j]);
+                                }
+                                running = false;
+                            }
+                            catch { Console.WriteLine("Ошибка чтения из файла. Проверьте файл для ввода: в нем должна быть матрица 7х7 из действительных чисел"); }
+                        }
+                        break;
+
+                    case "2":
+                        for (int i = 0; i < arr.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < arr.GetLength(1); j++)
+                            {
+                                bool ok = false;
+                                while (!ok)
+                                {
+                                    Console.WriteLine("Введите число в {0} строке и {1} столбце", i + 1, j + 1);
+                                    try
+                                    {
+                                        arr[i, j] = Convert.ToDouble(Console.ReadLine());
+                                        ok = true;
+                                    }
+                                    catch { Console.WriteLine("Ошибка ввода"); }
+                                }
+                            }
+                        }
+                        running = false;
+                        break;
+                }
+            }
+            Console.WriteLine("Полученная матрица: ");
+            for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                    bool ok = false;
-                    while (!ok)
-                    {
-                        Console.WriteLine("Введите число в {0} строке и {1} столбце", i + 1, j + 1);
-                        try
-                        {
-                            arr[i,j] = Convert.ToDouble(Console.ReadLine());
-                            ok = true;
-                        }
-                        catch { Console.WriteLine("Ошибка ввода"); }
-                    }
+                    Console.Write(arr[i, j] + "\t");
                 }
+                Console.WriteLine();
             }
 
             for (int i = 0; i < arr.GetLength(0); i++)
