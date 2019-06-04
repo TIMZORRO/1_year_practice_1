545589595f99721b20404d97db9336babe406426
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-06-04 17:28:43 +0500

    4 задача

--- Задача 4/Задача 4.cs
+++ Задача 4/Задача 4.cs
@@ -8,9 +8,39 @@ namespace Задача_4
 {
     class Program
     {
+        public static int EnterNut()
+        {
+            while (true)
+            {
+                try
+                {
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    if (a <= 0) throw new Exception();
+                    else return a;
+                }
+                catch { Console.WriteLine("Неверный ввод, повторите"); }
+            }
+        }
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Введите число, которое надо разложить");
+            int p = EnterNut();
+            Console.WriteLine("\nВведите число, на степени которого введенное ранее число нужно разложить");
+            int q = EnterNut();
+            int N = -1;
+            while (p >= Math.Pow(q, ++N)) ;
+            int[] arr_of_a = new int[N--];
+            for(int i = N; i > 0; i--)
+            {
+                while (p >= Math.Pow(q, i))
+                {
+                    p = (int)(p - Math.Pow(q, i));
+                    arr_of_a[i]++;
+                }
+            }
+            arr_of_a[0] = p;
+            for (int i = 0; i < arr_of_a.Length; i++) Console.WriteLine("A{0}: " + arr_of_a[i], i);
+            Console.ReadKey();
         }
     }
 }
