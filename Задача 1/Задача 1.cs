0b78e0ccdbc86292c3f41cf24d6751c0ab3d0d39
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-06-14 21:23:21 +0500

    Задача 1

--- Задача 1/Задача 1.cs
+++ Задача 1/Задача 1.cs
@@ -14,26 +14,23 @@ namespace Задача_1
             string input_f = "input.txt", output_f = "output.txt";
             using (FileStream sf = new FileStream(input_f, FileMode.OpenOrCreate)) { }
             int count = 0;
-            using (StreamReader reader = new StreamReader(input_f))
+            using (StreamReader reader = new StreamReader(input_f, Encoding.Default))
             {
                 Console.WriteLine("INPUT.TXT\n");
                 while (reader.Peek() > -1)
                 {
-                    string str = reader.ReadLine();
-                    Console.WriteLine(str);
-                    for (int i=0; i < str.Length; i++)
-                    {
-                        if (new char[] { '+', '-', '=', '\'', '"' }.Contains(str[i])) count += 3;
-                        else if (new char[] { '<', '>', '{', '}', '[', ']' }.Contains(str[i])) count += 8;
-                        else if (new char[] { '(', ')' }.Contains(str[i])) count += 1;
-                        else if (str[i] == '.') count += 5;
-                        else if (str[i] == ',') count += 2;
-                        else if (str[i] == ';') count += 7;
-                        else if (str[i] == ' ') count += 4;
-                        else if (str[i] >= 'a' && str[i] <= 'z') count += ((int)str[i] - (int)'a' + 1) / 10 + ((int)str[i] - (int)'a' + 1) % 10;
-                        else if (str[i] >= 'A' && str[i] <= 'Z') count += ((int)str[i] - (int)'A' + 1) / 10 + ((int)str[i] - (int)'A' + 1) % 10 + 10;
-                        else if (str[i] >= '0' && str[i] <= '9') count += 13 - ((int)str[i]-(int)'0');
-                    }
+                    char chr = (char)reader.Read();
+                    Console.Write(chr);
+                    if (new char[] { '+', '-', '=', '\'', '"' }.Contains(chr)) count += 3;
+                    else if (new char[] { '<', '>', '{', '}', '[', ']' }.Contains(chr)) count += 8;
+                    else if (new char[] { '(', ')' }.Contains(chr)) count += 1;
+                    else if (chr == '.') count += 5;
+                    else if (chr == ',') count += 2;
+                    else if (chr == ';') count += 7;
+                    else if (chr == ' ') count += 4;
+                    else if (chr >= 'a' && chr <= 'z') count += ((int)chr - (int)'a' + 1) / 10 + ((int)chr - (int)'a' + 1) % 10;
+                    else if (chr >= 'A' && chr <= 'Z') count += ((int)chr - (int)'A' + 1) / 10 + ((int)chr - (int)'A' + 1) % 10 + 10;
+                    else if (chr >= '0' && chr <= '9') count += 13 - ((int)chr - (int)'0');
                 }
             }
 
