ca4fcc804c8daffb218ff519c1ccb4908db9e6a9
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-06-19 16:16:06 +0500

    5 задача

--- Задача 5/Задача 5.cs
+++ Задача 5/Задача 5.cs
@@ -10,19 +10,36 @@ namespace Задача_5
 {
     class Program
     {
+        const int N = 7;
+
+        public static void Place (ref int x, ref int y, ref int dir)
+        {
+            switch (dir % 4)
+            {
+                case 0: // движение вправо
+                    y++;
+                    if (y == N - 1 - x) dir++;
+                    break;
+                case 1: // движение вниз
+                    x++;
+                    if (x == y) dir++;
+                    break;
+                case 2: // движение влево
+                    y--;
+                    if (y == N - 1 - x) dir++;
+                    break;
+                case 3: // движение вверх
+                    x--;
+                    if (x - 1 == y) dir++;
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
             string input_f = "input.txt";
-            int[,] spiral = new int[,] {    { 1, 2, 3, 4, 5, 6, 7 },
-                                            { 24, 25, 26, 27, 28, 29, 8 },
-                                            { 23, 40, 41, 42, 43, 30, 9 },
-                                            { 22, 39, 48, 49, 44, 31, 10 },
-                                            { 21, 38, 47, 46, 45, 32, 11 },
-                                            { 20, 37, 36, 35, 34, 33, 12 },
-                                            { 19, 18, 17, 16, 15, 14, 13 } };
 
-            double[,] arr = new double[7, 7];
-            double[] ans = new double[49];
+            double[,] arr = new double[N, N];
+            double[] ans = new double[(int)Math.Pow(N, 2)];
 
             bool running = true;
             while (running)
@@ -39,16 +56,20 @@ namespace Задача_5
                         {
                             try
                             {
-                                for (int i = 0; i < spiral.GetLength(0); i++)
+                                for (int i = 0; i < arr.GetLength(0); i++)
                                 {
                                     string str = reader.ReadLine();
                                     str = Regex.Replace(str.Trim(), @"\s+", " ");
-                                    string[] chisla = str.Split(new char[] { ' ' });
-                                    for (int j = 0; j < spiral.GetLength(1); j++) arr[i, j] = Convert.ToDouble(chisla[j]);
+                                    if (str != "")
+                                    {
+                                        string[] chisla = str.Split(new char[] { ' ' });
+                                        for (int j = 0; j < arr.GetLength(1); j++) arr[i, j] = Convert.ToDouble(chisla[j]);
+                                    }
+                                    else i--;
                                 }
                                 running = false;
                             }
-                            catch { Console.WriteLine("Ошибка чтения из файла. Проверьте файл для ввода: в нем должна быть матрица 7х7 из действительных чисел"); }
+                            catch { Console.WriteLine($"Ошибка чтения из файла. Проверьте файл для ввода: в нем должна быть матрица {N}х{N} из действительных чисел"); }
                         }
                         break;
 
@@ -56,24 +77,22 @@ namespace Задача_5
                         for (int i = 0; i < arr.GetLength(0); i++)
                         {
                             for (int j = 0; j < arr.GetLength(1); j++)
-                            {
-                                bool ok = false;
-                                while (!ok)
+                                while (true)
                                 {
                                     Console.WriteLine("Введите число в {0} строке и {1} столбце", i + 1, j + 1);
                                     try
                                     {
                                         arr[i, j] = Convert.ToDouble(Console.ReadLine());
-                                        ok = true;
+                                        break;
                                     }
                                     catch { Console.WriteLine("Ошибка ввода"); }
                                 }
-                            }
                         }
                         running = false;
                         break;
                 }
             }
+
             Console.WriteLine("Полученная матрица: ");
             for (int i = 0; i < arr.GetLength(0); i++)
             {
@@ -84,15 +103,14 @@ namespace Задача_5
                 Console.WriteLine();
             }
 
-            for (int i = 0; i < arr.GetLength(0); i++) // создание массива раскрученной спирали
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    ans[spiral[i, j] - 1] = arr[i, j];
-                }
-            }
+            int x = 0, y = 0, direction = 0;
 
-            for (int i = 0; i < ans.Length; i++) Console.WriteLine((i + 1) + " элемент спирали: " + ans[i]);
+            for (int i = 0; i < ans.Length; i++) // создание массива раскрученной спирали
+            {
+                ans[i] = arr[x, y];
+                Place(ref x, ref y, ref direction);
+                Console.WriteLine($"{i + 1} элемент спирали: {ans[i]}");
+            }
             Console.ReadKey();
         }
     }
