ad9a1256c8fe7649e40a69246f051d2b2d921922
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-06-21 01:29:57 +0500

    6 задача

--- Задача 6/Задача 6.cs
+++ Задача 6/Задача 6.cs
@@ -1,25 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Задача_6
 {
     class Program
     {
-        public static int count = 0;
-        public static double Recur (double a1, double a2, double a3, double M)
+        public static int Recur (double a1, double a2, double a3, double M, int N)
         {
             double a_next = a3 / 2 + a2 / 2 + a1 / 2;
 
             if (a_next <= M)
             {
                 Console.Write(a_next + " ");
-                count++;
-                return Recur(a2, a3, a_next, M);
+                N++;
+                if (a_next == M)
+                {
+                    Console.WriteLine("\nВыполняется ли равенство aN = M? Ответ: True");
+                    return N;
+                }
+                return Recur(a2, a3, a_next, M, N);
             }
-            else return a3;
+            Console.WriteLine("\nВыполняется ли равенство aN = M? Ответ: False");
+            return N;
         }
         public static double EnterDouble()
         {
@@ -43,12 +44,27 @@ namespace Задача_6
             Console.WriteLine("Введите число M: ");
             double M = EnterDouble();
 
-            Console.WriteLine("Полученная последовательность: ");
-            Console.Write(a1 + " " + a2 + " " + a3 + " ");
-            count = 3;
-            double a_n = Recur(a1, a2, a3, M);
-            Console.WriteLine("\nЕе длина: " + count);
-            Console.WriteLine("Выполняется ли равенство aN = M? Ответ: " + (a_n == M));
+            if (a1 == 0 && a2 == 0 && a3 == 0)
+                Console.WriteLine("Полученная последовательность является бесконечной последовательностью из нулей");
+
+            else if (a1 > M)
+                Console.WriteLine("Полученная последовательность пуста, так как первый член введенной последовательности больше M");
+
+            else if (a2 > M)
+                Console.WriteLine($"Полученная последовательность: {a1}\nВыполняется ли равенство aN = M? Ответ: {a1 == M}\nДлина полученной последовательности: 1");
+
+            else if (a3 > M)
+                Console.WriteLine($"Полученная последовательность: {a1} {a2}\nВыполняется ли равенство aN = M? Ответ: {a2 == M}\nДлина полученной последовательности: 2");
+
+            else
+            {
+                Console.WriteLine("Полученная последовательность: ");
+                Console.Write(a1 + " " + a2 + " " + a3 + " ");
+                int count = 3;
+                count = Recur(a1, a2, a3, M, count);
+                Console.WriteLine("\nДлина полученной последовательности: " + count);
+            }
+
             Console.ReadKey();
         }
     }
