23f8dd7a04e135a98edacdfd0fc2dc8ab3da4740
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-06-21 21:26:32 +0500

    9 задача

--- Задача 9/CircularList.cs
+++ Задача 9/CircularList.cs
@@ -1,37 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Задача_9
 {
-    class CircularListEntry
-    {
-        public int Value { get; set; }
-        public int Key { get; set; } // в коллекции является, по сути, индексатором
-        public CircularListEntry Next{ get; set; }
-        public CircularListEntry()
-        {
-            Key = 0;
-            Value = 0;
-            Next = null;
-        }
-        public CircularListEntry(int value)
-        {
-            Value = value;
-            Next = null;
-        }
-        public override string ToString()
-        {
-            return Convert.ToString(Value);
-        }
-        public void Show()
-        {
-            Console.WriteLine(ToString());
-        }
-    }
-
     class CircularList
     {
         public CircularListEntry First { get; set; }
---
+++ Задача 9/CircularListEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Задача_9
+{
+    class CircularListEntry
+    {
+        public int Value { get; set; }
+        public int Key { get; set; } // в коллекции является, по сути, индексатором
+        public CircularListEntry Next{ get; set; }
+        public CircularListEntry()
+        {
+            Key = 0;
+            Value = 0;
+            Next = null;
+        }
+        public CircularListEntry(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+        public override string ToString()
+        {
+            return Convert.ToString(Value);
+        }
+        public void Show()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
