0895140d397ec05e399314508dfece8e748d05b5
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-06-22 04:39:52 +0500

    10 задача

--- Задача 10/ListEntry.cs
+++ Задача 10/ListEntry.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Задача_10
+﻿namespace Задача_10
 {
     class ListEntry
     {
