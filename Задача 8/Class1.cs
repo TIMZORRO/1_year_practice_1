a834fd0329bb48ac09cd7c9c8b566a2b865becca
Тимофей Прибылев <tmpribylev_1@edu.hse.ru>
2019-06-23 19:21:20 +0500

    8 задача

--- Задача 8/Class1.cs
+++ Задача 8/Class1.cs
@@ -6,32 +6,35 @@ using System.Threading.Tasks;
 
 namespace Задача_8
 {
-    class VertexEntry : IComparable<VertexEntry>
+    class PointEntry 
     {
-        public int Degree { get; set; }
-        public List<VertexEntry> Near { get; set; }
-        public VertexEntry()
+        public int Degree
         {
-            Degree = 0;
+            get
+            {
+                return Near.Count;
+            }
+        }
+        public List<PointEntry> Near { get; set; }
+        public PointEntry()
+        {
             Near = null;
         }
-        public VertexEntry(ICollection<VertexEntry> c)
+        public PointEntry(ICollection<PointEntry> c)
         {
-            Degree = c.Count;
-            Near = new List<VertexEntry>(c);
+            Near = new List<PointEntry>(c);
         }
-        public VertexEntry(int degree)
+        public PointEntry(int degree)
         {
-            Degree = degree;
-            Near = new List<VertexEntry>(degree);
+            Near = new List<PointEntry>(degree);
         }
-        public int CompareTo(VertexEntry ve)
+        public int CompareTo(PointEntry ve)
         {
             if (this.Degree == ve.Degree) return 0;
             else if (this.Degree > ve.Degree) return 1;
             else return -1;
         }
-        public static bool Compare(VertexEntry ve1, VertexEntry ve2, int depth, int max_depth)
+        public static bool Compare(PointEntry ve1, PointEntry ve2, int depth, int max_depth)
         {
             if (max_depth == depth) return true;
             else
@@ -39,10 +42,10 @@ namespace Задача_8
                 bool ans = false;
                 if (ve1.Degree == ve2.Degree)
                 {
-                    foreach (VertexEntry venear1 in ve1.Near)
+                    foreach (PointEntry venear1 in ve1.Near)
                     {
                         bool help = false;
-                        foreach (VertexEntry venear2 in ve2.Near)
+                        foreach (PointEntry venear2 in ve2.Near)
                             help = Compare(venear1, venear2, depth + 1, max_depth) || help;
                         ans = ans && help;
                     }
